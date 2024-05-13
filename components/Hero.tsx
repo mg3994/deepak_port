df16757ce9e88c8bf211f42067c533bd6adbd47b
mg3994 <manishgautammg7@yahoo.com>
2024-05-13 23:13:09 +0530

    headernav

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className="flex justify-center flex-col w-[80%] h-[100%] mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-[3rem] items-center">
           <div>
-            <h1 className="text-[#c4cfde] mb-[1.3rem]">
+            <h1 className="text-[#c4cfde] mb-[1.3rem]"> 
               WELCOME TO MY PORTFOLIO
             </h1>
             <div>
